38bb3d758aea2c04c6ffb7e869303b698d9ae14d
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-06-22 20:02:07 +0300

    change slider

--- src/Components/Slider/Slider.jsx
+++ src/Components/Slider/Slider.jsx
@@ -1,18 +1,19 @@
 import React, { useRef, useState } from "react";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
-
+import BtnCallBack from "../BtnCallBack/BtnCallBack";
+import imgURL from "../../assets/MaestroProTransparent-1536x1209.png";
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
 
-import "./Slider.css";
+import "./Slider.scss";
 
 // import required modules
 import { Parallax, Pagination, Navigation } from "swiper";
 
-export default function App() {
+export default function Slider() {
   return (
     <>
       <Swiper
@@ -20,7 +21,7 @@ export default function App() {
           "--swiper-navigation-color": "#fff",
           "--swiper-pagination-color": "#fff",
           width: "1320px",
-          height: "600px",
+          height: "545px",
           backgroundColor: "inherit",
         }}
         speed={600}
@@ -41,22 +42,23 @@ export default function App() {
           }}
           data-swiper-parallax="-23%"></div>
         <SwiperSlide>
-          <div className="title" data-swiper-parallax="-300">
-            Slide 1
-          </div>
-          <div className="subtitle" data-swiper-parallax="-200">
-            Subtitle
-          </div>
-          <div className="text" data-swiper-parallax="-100">
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
-              dictum mattis velit, sit amet faucibus felis iaculis nec. Nulla
-              laoreet justo vitae porttitor porttitor. Suspendisse in sem justo.
-              Integer laoreet magna nec elit suscipit, ac laoreet nibh euismod.
-              Aliquam hendrerit lorem at elit facilisis rutrum. Ut at
-              ullamcorper velit. Nulla ligula nisi, imperdiet ut lacinia nec,
-              tincidunt ut libero. Aenean feugiat non eros quis feugiat.
-            </p>
+          <div className="container">
+            <div className="subtitle" data-swiper-parallax="-300">
+              <h1>Meltem</h1>
+              <p>
+                Meltem – це гарантовано якісне вентиляційне обладнання від
+                німецького заводу.
+              </p>
+              <BtnCallBack />
+            </div>
+            <div className="image" data-swiper-parallax="-200">
+              <img src={imgURL} alt="product" />
+            </div>
+            <div className="title" data-swiper-parallax="-100">
+              <p>Відцентровий витяжний вентилятор</p>
+              <h3>Meltem V-II 30-N</h3>
+              <BtnCallBack />
+            </div>
           </div>
         </SwiperSlide>
         <SwiperSlide>
