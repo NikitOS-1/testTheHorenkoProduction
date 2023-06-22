47ba5535fde5531dbe75f6cf9160c90c87014c96
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-06-22 16:29:04 +0300

    ref reset and fonts

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -1,6 +1,15 @@
 import style from "./App.module.scss";
+import Header from "./Header/Header";
+import Main from "./Main/Main";
+import Footer from "./Footer/Footer";
 
 const App = () => {
-  return <div className={style.app}>App</div>;
+  return (
+    <div className={style.app}>
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
 };
 export default App;
