e89450881d1310865a83e6cee96501e5d15eaed8
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-06-22 20:27:25 +0300

    new slider ref

---
+++ src/Components/Slider/Slide/Slide.jsx
@@ -0,0 +1,28 @@
+import BtnCallBack from "../../BtnCallBack/BtnCallBack";
+import "./Slide.scss";
+import imgURL from "../../../assets/MaestroProTransparent-1536x1209.png";
+
+const Slide = () => {
+  return (
+    <div className="container">
+      <div className="subtitle">
+        <h1>Meltem</h1>
+        <p>
+          Meltem – це гарантовано якісне вентиляційне обладнання від німецького
+          заводу.
+        </p>
+        <BtnCallBack />
+      </div>
+      <div className="image">
+        <img src={imgURL} alt="product" />
+      </div>
+      <div className="title">
+        <p>Відцентровий витяжний вентилятор</p>
+        <h3>Meltem V-II 30-N</h3>
+        <BtnCallBack />
+      </div>
+    </div>
+  );
+};
+
+export default Slide;
--- src/Components/Slider/Slider.jsx
+++ src/Components/Slider/Slider.jsx
@@ -1,160 +1,35 @@
 import React, { useRef, useState } from "react";
-// Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
-import BtnCallBack from "../BtnCallBack/BtnCallBack";
-import imgURL from "../../assets/MaestroProTransparent-1536x1209.png";
-// Import Swiper styles
+import { Navigation, Pagination } from "swiper";
+import Slide from "./Slide/Slide";
+
 import "swiper/css";
-import "swiper/css/pagination";
 import "swiper/css/navigation";
+import "swiper/css/pagination";
 
 import "./Slider.scss";
-
-// import required modules
-import { Parallax, Pagination, Navigation } from "swiper";
 
 export default function Slider() {
   return (
     <>
       <Swiper
-        style={{
-          "--swiper-navigation-color": "#fff",
-          "--swiper-pagination-color": "#fff",
-          width: "1320px",
-          height: "545px",
-          backgroundColor: "inherit",
-        }}
-        speed={600}
-        parallax={true}
+        navigation={false}
         pagination={{
           clickable: true,
         }}
-        navigation={true}
-        modules={[Parallax, Pagination, Navigation]}
+        modules={[Navigation, Pagination]}
         className="mySwiper">
-        <div
-          slot="container-start"
-          className="parallax-bg"
-          style={{
-            backgroundImage:
-              "url('https://images.wallpaperscraft.ru/image/single/lodka_gory_ozero_135258_3840x2160.jpg')",
-            backgroundSize: "100%",
-          }}
-          data-swiper-parallax="-23%"></div>
         <SwiperSlide>
-          <div className="container">
-            <div className="subtitle" data-swiper-parallax="-300">
-              <h1>Meltem</h1>
-              <p>
-                Meltem – це гарантовано якісне вентиляційне обладнання від
-                німецького заводу.
-              </p>
-              <BtnCallBack />
-            </div>
-            <div className="image" data-swiper-parallax="-200">
-              <img src={imgURL} alt="product" />
-            </div>
-            <div className="title" data-swiper-parallax="-100">
-              <p>Відцентровий витяжний вентилятор</p>
-              <h3>Meltem V-II 30-N</h3>
-              <BtnCallBack />
-            </div>
-          </div>
+          <Slide />
         </SwiperSlide>
         <SwiperSlide>
-          <div className="title" data-swiper-parallax="-300">
-            Slide 2
-          </div>
-          <div className="subtitle" data-swiper-parallax="-200">
-            Subtitle
-          </div>
-          <div className="text" data-swiper-parallax="-100">
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
-              dictum mattis velit, sit amet faucibus felis iaculis nec. Nulla
-              laoreet justo vitae porttitor porttitor. Suspendisse in sem justo.
-              Integer laoreet magna nec elit suscipit, ac laoreet nibh euismod.
-              Aliquam hendrerit lorem at elit facilisis rutrum. Ut at
-              ullamcorper velit. Nulla ligula nisi, imperdiet ut lacinia nec,
-              tincidunt ut libero. Aenean feugiat non eros quis feugiat.
-            </p>
-          </div>
+          <Slide />
         </SwiperSlide>
         <SwiperSlide>
-          <div className="title" data-swiper-parallax="-300">
-            Slide 3
-          </div>
-          <div className="subtitle" data-swiper-parallax="-200">
-            Subtitle
-          </div>
-          <div className="text" data-swiper-parallax="-100">
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
-              dictum mattis velit, sit amet faucibus felis iaculis nec. Nulla
-              laoreet justo vitae porttitor porttitor. Suspendisse in sem justo.
-              Integer laoreet magna nec elit suscipit, ac laoreet nibh euismod.
-              Aliquam hendrerit lorem at elit facilisis rutrum. Ut at
-              ullamcorper velit. Nulla ligula nisi, imperdiet ut lacinia nec,
-              tincidunt ut libero. Aenean feugiat non eros quis feugiat.
-            </p>
-          </div>
+          <Slide />
         </SwiperSlide>
         <SwiperSlide>
-          <div className="title" data-swiper-parallax="-300">
-            Slide 3
-          </div>
-          <div className="subtitle" data-swiper-parallax="-200">
-            Subtitle
-          </div>
-          <div className="text" data-swiper-parallax="-100">
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
-              dictum mattis velit, sit amet faucibus felis iaculis nec. Nulla
-              laoreet justo vitae porttitor porttitor. Suspendisse in sem justo.
-              Integer laoreet magna nec elit suscipit, ac laoreet nibh euismod.
-              Aliquam hendrerit lorem at elit facilisis rutrum. Ut at
-              ullamcorper velit. Nulla ligula nisi, imperdiet ut lacinia nec,
-              tincidunt ut libero. Aenean feugiat non eros quis feugiat.
-            </p>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="title" data-swiper-parallax="-300">
-            Slide 3
-          </div>
-          <div className="subtitle" data-swiper-parallax="-200">
-            Subtitle
-          </div>
-          <div className="text" data-swiper-parallax="-100">
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
-              dictum mattis velit, sit amet faucibus felis iaculis nec. Nulla
-              laoreet justo vitae porttitor porttitor. Suspendisse in sem justo.
-              Integer laoreet magna nec elit suscipit, ac laoreet nibh euismod.
-              Aliquam hendrerit lorem at elit facilisis rutrum. Ut at
-              ullamcorper velit. Nulla ligula nisi, imperdiet ut lacinia nec,
-              tincidunt ut libero. Aenean feugiat non eros quis feugiat.
-            </p>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="title" data-swiper-parallax="-300">
-            Slide 3
-          </div>
-          <div className="subtitle" data-swiper-parallax="-200">
-            Subtitle
-          </div>
-          <div className="text" data-swiper-parallax="-100">
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
-              dictum mattis velit, sit amet faucibus felis iaculis nec. Nulla
-              laoreet justo vitae porttitor porttitor. Suspendisse in sem justo.
-              Integer laoreet magna nec elit suscipit, ac laoreet nibh euismod.
-              Aliquam hendrerit lorem at elit facilisis rutrum. Ut at
-              ullamcorper velit. Nulla ligula nisi, imperdiet ut lacinia nec,
-              tincidunt ut libero. Aenean feugiat non eros quis feugiat.
-            </p>
-          </div>
+          <Slide />
         </SwiperSlide>
       </Swiper>
     </>
