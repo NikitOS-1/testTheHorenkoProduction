7820af28144654313b8f24306eba9fd4240a49d5
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-06-23 16:47:29 +0300

    compleated form layout

--- src/Components/Modal/Modal.jsx
+++ src/Components/Modal/Modal.jsx
@@ -1,15 +1,38 @@
 import style from "./Modal.module.scss";
+import flag from "../../assets/flagUA.png";
 
 const Modal = ({ isActive, setIsActive }) => {
   return (
     <div className={[style["container"], style[`${isActive}`]].join(" ")}>
-      {/* // <div className={style.container}> */}
-      <div
-        onClick={() => setIsActive("notActive")}
-        style={{ width: "50px", height: "50px", backgroundColor: "tomato" }}>
-        X
+      <div onClick={() => setIsActive("notActive")} className={style.btnClose}>
+        <div className={style.bar1} />
+        <div className={style.bar2} />
       </div>
-      Modal <form action=""></form>
+      <div className={style.wrap}>
+        <div className={style.title}>
+          <h1>Залишайте заявку</h1>
+          <p>Ми зв’яжемося з вами найближчим часом.</p>
+        </div>
+        <div className={style.form}>
+          <form action="#">
+            <div className={style.name}>
+              <input type="text" placeholder="Ваше ім’я*" />
+            </div>
+            <div className={style.number}>
+              <img src={flag} alt="Flag" />
+              <input type="text" placeholder="+380-00-000-00-00*" />
+            </div>
+            <button className={style.btnSent}>Відправить</button>
+          </form>
+          <p className={style.accept}>
+            Натискаючи кнопку “Надіслати”, ви погоджуєтесь з{" "}
+            <span style={{ textDecoration: "underline" }}>
+              Правилами обробки персональних даних
+            </span>
+            .
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
